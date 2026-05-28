9aab0fcf6c70522a6ceb1711511379a640092f2f
Saroyar <rajmolll2000@gmail.com>
2026-05-28 10:37:59 +0530

    user can book a slot

--- Medbackend/Backend/src/main/java/in/medhub/backend/model/User.java
+++ Medbackend/Backend/src/main/java/in/medhub/backend/model/User.java
@@ -19,7 +19,9 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String userName;
+	private String email;
 	private String password;
+	private String address;
 	public Long getId() {
 		return id;
 	}
@@ -38,6 +40,19 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
 	
 	
 }
